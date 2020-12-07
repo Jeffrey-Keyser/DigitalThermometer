9e6a4fd4d00c738c02d2a3626a8c95db6802c269
JeffPC <JeffPC@DESKTOP-IAG8NNU>
2020-12-06 20:38:47 -0600

    Add symptoms to negative reading

--- app/src/main/java/com/example/digitalthermometer/MainActivity.java
+++ app/src/main/java/com/example/digitalthermometer/MainActivity.java
@@ -1,38 +1,41 @@
 package com.example.digitalthermometer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button readingsActivity, thermalActivity;
-
+    private ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get buttons
-        readingsActivity = (Button) findViewById(R.id.database_activity_btn);
-        thermalActivity = (Button) findViewById(R.id.capture_activity_btn);
-
-        readingsActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ReadingsActivity.class));
+        constraintLayout = (ConstraintLayout) findViewById(R.id.main_activity_layout);
+        constraintLayout.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+                //Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();
             }
-        });
-
-        thermalActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            public void onSwipeRight() {
+                //Toast.makeText(MainActivity.this, "capture", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(MainActivity.this, ThermalActivity.class));
             }
-        });
+            public void onSwipeLeft() {
+                //Toast.makeText(MainActivity.this, " database", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, ReadingsActivity.class));
+            }
+            public void onSwipeBottom() {
+               // Toast.makeText(MainActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
 
+        });
     }
 }
